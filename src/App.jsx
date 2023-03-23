2234a91a0332e0b0c79f9a0fa1c8b5ce0b47ae11
Nairita Paul <nairitapaul10@gmail.com>
2023-03-23 18:26:35 +0800

    Render a part of my-moviebox app with hardcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import React from "react";
 import "./App.css";
+import img1 from "./assets/pic1.jpg";
+// import Input from "./Input";
+function inputBtnClick() {}
 
 function App() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <main>
+      <input
+        className="inputField"
+        type="text"
+        placeholder="Enter your favourite movie !"
+      />
+      <button className="input-btn" onClick={inputBtnClick}>
+        Search here
+      </button>
+      <ul>
+        <li>
+          <img src={img1} alt="fav-movie" className="movie-poster-img"></img>
+          <p>Title : Adventour of life</p>
+          <p>Genre : Travel</p>
+          <p>Director : A K Thomson</p>
+        </li>
+        <li>
+          <img src={img1} alt="fav-movie" className="movie-poster-img"></img>
+          <p>Title : Journey of Life</p>
+          <p>Genre : Travel</p>
+          <p>Director : A K Thomson</p>
+        </li>
+      </ul>
+    </main>
   );
 }
 
